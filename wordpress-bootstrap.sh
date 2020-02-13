993bd5890138f96b9225dcf494625b4851dae6ae
mmlakin <49107138+mmlakin@users.noreply.github.com>
2020-02-12 23:19:47 -0800

    Create wordpress-bootstrap.sh
    Copied from updated comment at https://gist.github.com/mikepfeiffer/00a746ef274b62bf506e8f8602e03333

---
+++ wordpress-bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum update -y
+yum -y install httpd mysql
+amazon-linux-extras install epel -y
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum-config-manager --disable remi-php54
+yum-config-manager --enable remi-php73
+amazon-linux-extras install php7.3 -y
+service httpd start
+chkconfig httpd on
+echo "fs-ffd1b734.efs.eu-west-1.amazonaws.com:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
+mount -a
+systemctl restart httpd
