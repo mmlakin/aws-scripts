99e96ddd92ce89c1f5c602c0457bd664ccabb9e8
mmlakin <49107138+mmlakin@users.noreply.github.com>
2020-02-13 01:53:33 -0800

    Delete apache.sh

--- apache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum -y install httpd mysql
-amazon-linux-extras install epel -y
-yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum-config-manager --disable remi-php54
-yum-config-manager --enable remi-php73
-amazon-linux-extras install php7.3 -y
