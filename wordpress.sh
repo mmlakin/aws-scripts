284e6bf9795d17eb2f2c011a10bad3260b150a5c
mmlakin <49107138+mmlakin@users.noreply.github.com>
2020-02-13 01:55:43 -0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install Wordpress to /var/www/html/
+# Install Wordpress to EFS share mounted at /var/www/html/
 wget https://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
 cp -r wordpress/* /var/www/html/
